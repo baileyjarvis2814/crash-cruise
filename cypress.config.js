a036fa7193eba4d4b2a212ec6deffccb6ef3db14
baileyjarvis2814 <baileyjarvis2814@gmail.com>
2024-08-04 13:23:42 -0600

    debugging

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,6 @@
-const { defineConfig } = require('cypress');
-
-module.exports = defineConfig({
+// cypress.config.js
+module.exports = {
   e2e: {
     baseUrl: 'https://crash-cruise-hqe3fizo2-jarvis-projects-77e68e1c.vercel.app',
-    defaultCommandTimeout: 10000,
-    pageLoadTimeout: 30000,
-    setupNodeEvents(on, config) {
-      // Node event listeners can be implemented here if needed.
-    },
   },
-});
+}
